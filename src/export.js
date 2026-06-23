6421167f3d6650177d75b5985d5d0540b3001c87
beardev-in <abhinav.edulakanti@gmail.com>
2026-06-23 16:27:13 +0530

    Add CSV header support

--- src/export.js
+++ src/export.js
@@ -1,6 +1,12 @@
 function exportCSV(metrics) {
- const rows = metrics.map( row => `${row.page},${row.visits}` );
- return "";
+  const rows = [
+    "page,visits",
+    ...metrics.map(
+      row => `${row.page},${row.visits}`
+    )
+  ];
+
+  return "";
 }
 
 module.exports = exportCSV;
